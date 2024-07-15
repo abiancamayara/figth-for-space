7b11ceaf99087883f9a3ec37ebd0dd3d59924b7e
abiancamayara <bianca.mayara@escolar.ifrn.edu.br>
2024-07-15 16:21:05 -0300

    programando o laser

--- figth for space/Assets/Script/PlayerLuna.cs
+++ figth for space/Assets/Script/PlayerLuna.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class PlayerLuna : MonoBehaviour
 {
     public Rigidbody2D rig;
     public GameObject laserDoJogador;
@@ -16,13 +16,14 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        temLaserDuplo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovimentarJogador();
+        AtirarLaser();
     }
 
 
@@ -34,9 +35,12 @@ public class PlayerManager : MonoBehaviour
 
     private void AtirarLaser()
     {
-        if(temLaserDuplo == false)
+        if(Input.GetButtonDown("Fire1"))
         {
-
+            if(temLaserDuplo == false)
+        {
+            Instantiate(laserDoJogador, localDoDisparoUnico.position, localDoDisparoUnico.rotation);
+        }
         }
 
     }
