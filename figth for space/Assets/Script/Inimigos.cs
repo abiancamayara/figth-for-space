bbf5cfc083d6d22ddd2a4409f6c9da9b370d5983
abiancamayara <bianca.mayara@escolar.ifrn.edu.br>
2024-07-17 09:01:33 -0300

    add os inimigos

---
+++ figth for space/Assets/Script/Inimigos.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigos : MonoBehaviour
+{
+    public float velocidadeDoInimigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarInimigo();
+    }
+
+    private void MovimentarInimigo()
+    {
+        transform.Translate(Vector3.down * velocidadeDoInimigo * Time.deltaTime);
+    }
+}
