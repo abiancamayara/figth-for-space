20232c9aeedcff4e3768dba5df596204646b7d00
abiancamayara <bianca.mayara@escolar.ifrn.edu.br>
2024-07-24 09:24:43 -0300

    ajustando a vida do player

---
+++ figth for space/Assets/Script/VidaDosPlayers.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaDosPlayers : MonoBehaviour
+{
+    public int vidaMaximaDoJogador;
+    public int vidaAtualDojogador;
+
+    public bool temEscudo; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MachucarJogador(int danoParaReceber)
+    {
+        if(temEscudo == false)
+        {
+            vidaAtualDojogador -= danoParaReceber;
+
+            if(vidaAtualDojogador <= 0)
+            {
+                
+            }
+        }
+    }
+}
