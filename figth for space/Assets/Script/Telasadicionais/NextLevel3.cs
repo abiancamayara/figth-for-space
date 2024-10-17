c24ad39979ed90b3a7f71ce48d401d7218538078
BIANCA MAYARA NASCIMENTO DE SOUZA <bianca.mayara@academico.ifrn.edu.br>
2024-10-17 11:10:09 -0300

    ta jogavel?

---
+++ figth for space/Assets/Script/Telasadicionais/NextLevel3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel3 : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(7);
+    }
+}
